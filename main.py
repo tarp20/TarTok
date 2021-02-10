6378c65925a38eed7d635d36e78ff75dd6429fc9
Taras Piont <taras.piont@gmail.com>
2021-02-10 17:38:56 +0100

    first steps

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import shutil
+from typing import List
 from fastapi import FastAPI, UploadFile, File
 
 
@@ -10,4 +11,12 @@ async def root(file: UploadFile = File(...)):
     with open(f'{file.filename}', 'wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
     return {'file_name ': file.filename}
+
+
+@app.post('/img')
+async def upload_image(files: List[UploadFile] = File(...)):
+    for img in files:
+        with open(f'{img.filename}', 'wb') as buffer:
+            shutil.copyfileobj(img.file, buffer)
+    return {'file_name ': 'Good'}
  
